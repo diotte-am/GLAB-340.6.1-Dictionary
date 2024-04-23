207b7a2fc5b48118be9ade4461f33f8aa08d1a2c
Amare Diotte <83765247+diotte-am@users.noreply.github.com>
2024-04-23 16:18:39 -0400

    Commit to git

---
+++ diotte_mdms_dic.py
@@ -0,0 +1,17 @@
+'''
+Amare Diotte
+Data Engineering
+2024-RTT-107
+GLAB-340.6.1: Python Dictionary
+'''
+
+import typing
+
+def main_loop():
+    movie_db: {str} = {}
+
+    print(movie_db)
+
+
+
+main_loop()
